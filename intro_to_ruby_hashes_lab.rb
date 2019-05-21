82adc259ca701bac996027bacfda11beb1227513
Marc Poppy <poppy.marc@gmail.com>
2019-05-21 04:49:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,9 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :names => {}
+      :names => {
+        :rent_in_dollars => {}
+      }
     }
   }
 end
